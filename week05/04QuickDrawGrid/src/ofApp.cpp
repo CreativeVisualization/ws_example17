57b23dd64e198aaeee5a6a392d04e7aa1fbb0205
Atsushi Tadokoro <tadokoro@gmail.com>
2017-05-20 14:13:51 +0900

    update

---
+++ week05/04QuickDrawGrid/src/ofApp.cpp
@@ -0,0 +1,61 @@
+#include "ofApp.h"
+
+void ofApp::setup(){
+    ofBackground(255);
+    ofSetBackgroundAuto(false);
+    ofSetColor(0);
+    ofNoFill();
+    ofSetLineWidth(1.0);
+    //ファイルを指定
+    string inputFile = "face.ndjson";
+    ifstream input(ofToDataPath(inputFile), ifstream::in);
+    //JSONファイルを1行ずつ読み込む
+    ofxJSONElement jsonElement;
+    Json::Reader reader;
+    string currentLine;
+    bool success = true;
+    while(success) {
+        getline(input, currentLine);
+        success = reader.parse(currentLine, jsonElement);
+        //読み込んだJSONデータを配列に追加
+        drawDataJson.push_back(jsonElement);
+    }
+    cout << "Done : read " << drawDataJson.size() << " drawings!"<< endl;
+    objectNum = 0;
+}
+
+void ofApp::update(){
+}
+
+void ofApp::draw(){
+    //表示スケール
+    float scale = 0.075;
+    //イラストのサイズ(幅・高さ)
+    float size = 256 * scale;
+    //列の数
+    int col = ofGetWidth() / size + 1;
+    //行の数
+    int row = ofGetHeight() / size + 1;
+    //グリッド状にイラストを配置
+    for (int i = 0; i < col * row; i++) {
+        //表示位置
+        ofVec2f location;
+        location.x = i % col * size;
+        location.y = i / col * size;
+        //描画設定
+        ofPushMatrix();
+        //位置を中心に
+        ofTranslate(location.x, location.y);
+        //JSONの配列からイラストを再現
+        for (int j = 0; j < drawDataJson[i]["drawing"].size(); j++) {
+            ofBeginShape();
+            for (int k = 0; k < drawDataJson[i]["drawing"][j][0].size(); k++) {
+                int x = drawDataJson[i]["drawing"][j][0][k].asInt();
+                int y = drawDataJson[i]["drawing"][j][1][k].asInt();
+                ofVertex(x * scale, y * scale);
+            }
+            ofEndShape();
+        }
+        ofPopMatrix();
+    }
+}
