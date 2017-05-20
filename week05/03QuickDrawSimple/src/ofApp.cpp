601cb801fc63ba46e99c8b845b18dc1c5e0a4491
Atsushi Tadokoro <tadokoro@gmail.com>
2017-05-20 13:25:37 +0900

    update

---
+++ week05/03QuickDrawSimple/src/ofApp.cpp
@@ -0,0 +1,47 @@
+#include "ofApp.h"
+
+void ofApp::setup(){
+    ofBackground(255);
+    ofSetColor(0);
+    objectNum = 0;
+    //ファイルを指定
+    string inputFile = "face.ndjson";
+    ifstream input(ofToDataPath(inputFile), ifstream::in);
+    //JSONファイルを1行ずつ読み込む
+    ofxJSONElement jsonElement;
+    Json::Reader reader;
+    string currentLine;
+    bool success = true;
+    while(success) {
+        getline(input, currentLine);
+        success = reader.parse(currentLine, jsonElement);
+        //読み込んだJSONデータを配列に追加
+        drawDataJson.push_back(jsonElement);
+    }
+    cout << "Done : read " << drawDataJson.size() << " drawings!"<< endl;
+}
+
+void ofApp::update(){
+}
+
+void ofApp::draw(){
+    //フレーム番号から表示するオブジェクトを決定
+    objectNum = ofGetFrameNum()/8;
+    //描画設定
+    ofNoFill();
+    ofSetLineWidth(2.0);
+    ofPushMatrix();
+    //位置を中心に
+    ofTranslate(ofGetWidth()/2 - 128, ofGetHeight()/2 - 128);
+    //JSONの配列からイラストを再現
+    for (int j = 0; j < drawDataJson[objectNum]["drawing"].size(); j++) {
+        ofBeginShape();
+        for (int k = 0; k < drawDataJson[objectNum]["drawing"][j][0].size(); k++) {
+            int x = drawDataJson[objectNum]["drawing"][j][0][k].asInt();
+            int y = drawDataJson[objectNum]["drawing"][j][1][k].asInt();
+            ofVertex(x, y);
+        }
+        ofEndShape();
+    }
+    ofPopMatrix();
+}
