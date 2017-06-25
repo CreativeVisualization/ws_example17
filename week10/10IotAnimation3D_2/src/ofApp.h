f737989da646432497f72841c97ab141b175e67a
Atsushi Tadokoro <tadokoro@gmail.com>
2017-06-25 20:45:21 +0900

    update

---
+++ week10/10IotAnimation3D_2/src/ofApp.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxCsv.h"
+
+class ofApp : public ofBaseApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    ofxCsv csv;
+    vector<string> timeStamp;
+    vector<float> temperature;
+    vector<float> humidityAir;
+    vector<float> pressure;
+    vector<float> illuminance;
+    vector<float> humiditySoil;
+    ofColor color[5];
+    
+    ofEasyCam cam;
+    ofLight light;
+    vector<ofVec3f> location;
+    vector<float> radius;
+    int drawLength;
+};
