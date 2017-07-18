fe1b4385f7302f9b5a84a8c0a4938b7d280782aa
Atsushi Tadokoro <tadokoro@gmail.com>
2017-07-18 15:33:51 +0900

    update

---
+++ exhibition_example/TidalMonitor/addons.make
@@ -0,0 +1,3 @@
+ofxOsc
+ofxStateMachine
+ofxThreadedOSCReceiver
---
+++ exhibition_example/TidalMonitor/src/ofxTidalView.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOsc.h"
+#include "ofxThreadedOSCReceiver.h"
+
+class TidalNote {
+public:
+    int instNum;
+    string instName;
+    int beatCount;
+    int syncCount;
+};
+
+
+class ofxTidalView {
+public:
+    ofxTidalView(int port);
+    void oscReceiveEvent(ofxOscMessage &m);
+    void beatShift();
+    void beatMonitor();
+    void calcStat();
+    void instCleanup();
+    void updateCycle();
+    
+    ofxThreadedOSCReceiver receiver;
+    vector<string> instNames;
+    vector<string> instBuffer;
+    vector<TidalNote> notes;
+    
+    float cycle;
+    float lastCycle;
+    int resolution;
+    int noteCount;
+    bool updated;
+    
+    static const int max1 = 128;
+    static const int max2 = 64;
+    int noteMatrix[max1][max2];
+    float syncopation[max1];
+    double entropy[max1];
+    double jointEntropy[max1];
+    double mutualInformation[max1];
+    int noteNum[max1];
+    int instNumMax;
+};
